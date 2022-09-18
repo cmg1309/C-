9b10c993f86548db5dc977a4e55e14bf5dcf182c
Mingi Cha <cmg1309@gmail.com>
2022-09-18 19:43:00 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System; 
+//class HelloWorld
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Hello World!");
+//    }
+//}
+
+using System;
+class HelloWorld
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}

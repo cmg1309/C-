2fea8af2a8741f2fb20fafccb0434e3016a9f5b8
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-22 20:56:03 +0900

    Update 실습과제 1번.c

--- 실습과제 1번.c
+++ 실습과제 1번.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	double n1 = 0, n2 = 0;
 
-	printf("�Ǽ�1 �Է�?:");
+	printf("½Ç¼ö1 ÀÔ·Â?:");
 	scanf_s("%lf", &n1);
-	printf("�Ǽ�2 �Է�?:");
+	printf("½Ç¼ö2 ÀÔ·Â?:"); 
 	scanf_s("%lf", &n2);
 	printf("%7.2lf / %7.2lf = %15.2lf", n1, n2, n1 / n2);
 

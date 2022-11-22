77facc93f060503e01ad4ae9f99b69928b32aadb
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-22 21:02:50 +0900

    Add files via upload

---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                fontDialog1.ShowApply = true;
+            }
+            else if(checkBox1.Checked == false)
+            {
+                fontDialog1.ShowApply = false;
+            }
+
+            if(checkBox2.Checked)
+            {
+                fontDialog1.ShowColor = true;
+            }
+            else if (checkBox2.Checked == false)
+            {
+                fontDialog1.ShowColor = false;
+            }
+
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                fontDialog1.ShowApply = true;
+                fontDialog1.ShowColor = true;
+            }
+
+            fontDialog1.ShowDialog();
+        }
+    }
+}

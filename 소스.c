41cf9fb503903aab5aecf74ba3a0e78ea1ff95da
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-28 22:51:08 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,47 +1,119 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<time.h>
+//// type char  1,2 가 아닌 숫자는 잘못된 숫자
+//
+//#define _CRT_SECURE_NO_WARNINGS                      // 첫행에 와야함
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int point = 0;
+//	char type = 0;
+//	// 정수나 실수, 문자를 입력 받은 후에 두번째 scanf_s 에서 문자형태의 데이터를 입력받으면 저장하지 못하고 넘어감.
+//	printf("필기시험점수 입력:");
+//	scanf_s("%d", &point);                           // enter키 = '\n'  
+//	//getchar();  -> 첫번째 해결법
+//	printf("번호를 선택: 1(1종먼허 ),2(2종먼허):");
+//	scanf("\n%c", &type, 1);                         // 두번째 해결법 : %c 앞에 \n을 넣음
+//
+//	if (type == '1')
+//	{
+//		if (point >= 70)
+//			printf("1종 면허 합격");
+//		else
+//			printf("1종 면허 불합격");
+//	}
+//	else if (type == '2')                           // type 1 2 가 아닌게 있어서 else를 붙히는게 좋음
+//	{
+//		if (point >= 60)
+//			printf("2종 면허 합격");
+//		else
+//			printf("2종 면허 불합격");
+//	}
+//	else
+//	{
+//		printf("잘못된 숫자 입니다.\n");
+//	}
+//
+//	return 0;
+//}
 
-#define FILE_NAME "test.txt"
-#define BUF_SIZE 1024
+
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	double n1 = 0, n2 = 0, n3 = 0;
+//	double result = 0;
+//
+//	printf("성적입력:");
+//	scanf_s("%lf %lf %lf", &n1, &n2, &n3);
+//
+//	if ((a < 0 || a > 0) || (b < 0 || b > 100) || (c < 0 || c > 100))
+//	{
+//		printf("성적은 0~100점까지만 입력가능합니다.\n\n");
+//		return 0;
+//	}
+//
+//	result = (n1 + n2 + n3) / 3;
+//
+//	if (result > 90) 
+//	{
+//		printf("평균: %7.2f\n 평점 A는 전액장학금이 지급됩니다.\n\n", result);
+//	}
+//	else if (result > 80) 
+//	{
+//		printf("평균: %7.2f\n 평점 B는 전액장학금이 지급됩니다.\n\n", result);
+//	}
+//	else 
+//	{
+//		printf("평균: %7.2f는 장학금을 받을 수 없습니다.\n\n", result);
+//	}
+//}
+
+
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	double n1 = 0, n2 = 0, n3 = 0, result = 0;
+//	int con = 0;
+//
+//	printf("성적입력:");
+//	scanf_s("%f %f %f", &n1, &n2, &n3);
+//	if ((a < 0 || a > 0) || (b < 0 || b > 100) || (c < 0 || c > 100))
+//	{
+//		printf("성적은 0~100점까지만 입력가능합니다.\n\n");
+//		return 0;
+//	}
+//	con = (int)result / 10;
+//
+//	switch (con)
+//	{
+//	case 9:, case 10:
+//		printf("평균: %7.2f\n 평점 A는 전액장학금이 지급됩니다.\n\n", result);
+//		break;
+//	case 8:
+//		printf("평균: %7.2f\n 평점 B는 전액장학금이 지급됩니다.\n\n", result);
+//		break;
+//	default:
+//		printf("평균: %7.2f는 장학금을 받을 수 없습니다.\n\n", result);
+//	}
+//}
+
+
+// 두 정수를 입력 받아 앞의 정수가 크면 두 수를 더하고, 뒤의 정수가 크거나 같으면 두 수를 곱하여 결과를 출력하는 프로그램을 작성
+
+#include<stdio.h>
 
 int main(void)
 {
-	char write_buf[BUF_SIZE] = "Hello world-test file";
-	char read_buf[BUF_SIZE] = "";
-	clock_t start1, end1;
-	float res1;
+	signed int x = 0, y = 0;
 
-	FILE* fp = NULL;
+	printf("두 정수 입력:");
+	scanf_s("%d %d", &x, &y);
 
-	start1 = clock();
-
-	fp = fopen(FILE_NAME, "r");
-	if (fp == NULL)
-	{
-		printf("open error\n");
-		return 1;
-	}
-	fread(read_buf, 16, sizeof(write_buf), fp);
-	printf("test.txt 파일 읽은내용 = [%s]\n", read_buf);
-	fclose(fp);
-
-	fp = fopen(FILE_NAME, "w");
-	//fp = fopen("result.txt", "w");
-	if (fp == NULL)
-	{
-		printf("open error\n");
-		return 1;
-	} 
-	fwrite(write_buf, sizeof(char), sizeof(write_buf), fp);
-	fclose(fp);
-
-	end1 = clock();
-
-	res1 = (float)(end1 - start1) / CLOCKS_PER_SEC;
-	printf(" 소요된 시간 : %.3f \n", res1);
-	printf(" 시작 시간= %d \n", start1);
-	printf(" 종료 시간 = %d \n", end1);
-
+	if (x > y)
+		printf("더한 결과 값:%d + %d = %d\n", x, y, x + y);
+	else
+		printf("곱한 결과 값:%d * %d = %d\n", x, y, x * y);
 	return 0;
 }

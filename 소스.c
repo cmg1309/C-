35f414666d819285ce3b38a7ca877df18399e6f1
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-28 22:52:47 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,21 +1,248 @@
+// 실습예제 7-3
+//#include<stdio.h>
+//
+//#define ROWSIZE 2
+//#define COLSIZE 3
+//
+//int main(void)
+//{
+//	int i = 0, j = 0;
+//	int sum = 0;
+//
+//	int td[ROWSIZE][COLSIZE] = { 0 };
+//
+//	printf("6개의 데이터 입력 : ");
+//	for (i = 0; i < ROWSIZE; i++)
+//	{
+//		for (j = 0; j < COLSIZE; j++)
+//			scanf_s("%d", &td[i][j]);
+//	}
+//
+//	for (i = 0; i < ROWSIZE; i++)
+//	{
+//		for (j = 0; j < COLSIZE; j++)
+//			sum += td[i][j];
+//	}
+//	printf("합 : %d, 평균 : %lf\n", sum, (double)sum / (double)(ROWSIZE * COLSIZE));
+//
+//	printf("반복문 for를 이용하여 출력\n");
+//	for (i = 0; i < ROWSIZE; i++)
+//	{
+//		for (j = 0; j < COLSIZE; j++)
+//			printf("%d ", td[i][j]);
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+// 실습예제 7-3 확장문제
+//#include<stdio.h>
+//
+//int tot(int data[2][3], int ROWSIZE, int COLSIZE);
+//void output(int data[2][3], int ROWSIZE, int COLSIZE);
+//void input(int data[2][3], int ROWSIZE, int COLSIZE);
+//
+//int main(void)
+//{
+//	int i = 0, j = 0, sum = 0, ROWSIZE = 0, COLSIZE = 0;
+//
+//	int data[2][3] = { 0 };
+//	printf("6개의 데이터 입력 : ");
+//	ROWSIZE = sizeof(data) / sizeof(data[0]);
+//	COLSIZE = sizeof(data[0]) / sizeof(data[0][0]);
+//
+//	input(data, ROWSIZE, COLSIZE);
+//	sum = tot(data, ROWSIZE, COLSIZE);
+//	printf("합: %d, 평균: %f\n", sum, (double)sum / ((double)ROWSIZE * COLSIZE));
+//	printf("반복문 for를 이용하여 출력\n");
+//	output(data, ROWSIZE, COLSIZE);
+//
+//	return 0;
+//}
+//
+//void input(int data[2][3], int ROWSIZE, int COLSIZE)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < ROWSIZE; i++)
+//		for (j = 0; j < COLSIZE; j++)
+//			scanf_s("%d", &data[i][j]);
+//}
+//
+//void output(int data[2][3], int ROWSIZE, int COLSIZE)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < ROWSIZE; i++)
+//	{
+//		for (j = 0; j < COLSIZE; j++)
+//		{
+//			printf("%d ", data[i][j]);
+//		}
+//		puts("");
+//	}
+//}
+//
+//int tot(int data[2][3], int ROWSIZE, int COLSIZE)
+//{
+//	int i = 0, j = 0;
+//	int sum = 0;
+//	for (i = 0; i < ROWSIZE; i++)
+//		for (j = 0; j < COLSIZE; j++)
+//			sum += data[i][j];
+//	return sum;
+//}
+
+// 실습예제 7-5
+//#include<stdio.h>                
+//
+//#define ROWSIZE 4
+//#define COLSIZE 2
+//
+//void input(int score[ROWSIZE][COLSIZE]);
+//void output(int score[ROWSIZE][COLSIZE], int sum, int midsum, int finalsum);
+//
+//int main(void)
+//{
+//	int i = 0, j = 0, sum = 0, midsum = 0, finalsum = 0;
+//
+//	int score[ROWSIZE][COLSIZE] = { 0 };
+//
+//	printf("중간, 기말 성적을 입력하세요: ");
+//	for (i = 0; i < ROWSIZE; i++)
+//		for (j = 0; j < COLSIZE; j++)
+//			scanf_s("%d", &score[i][j]);
+//	input(score);
+//
+//	printf("       중간       기말     \n");
+//	printf("---------------------------\n");
+//	for (i = 0; i < ROWSIZE; i++)
+//	{
+//		for (j = 0; j < COLSIZE; j++)
+//		{
+//			printf("%10d", score[i][j]);
+//			sum += score[i][j];
+//			if (j == 0) midsum += score[i][j];
+//			else finalsum += score[i][j];
+//		}
+//		puts("");
+//	}
+//
+//	printf("---------------------------\n");
+//	printf("\n평균: %7.2f %7.2f\n", (double)midsum / ROWSIZE, (double)finalsum / ROWSIZE);
+//	printf("성적의 합은 %d이고 평균은 %.2f이다\n", sum, (double)sum / (ROWSIZE * COLSIZE));
+//	output(score, sum, midsum, finalsum);
+//
+//	return 0;
+//}
+//
+//void input(int score[ROWSIZE][COLSIZE])
+//{
+//	int i = 0, j = 0;
+//	printf("중간, 기말 성적을 입력하세요: ");
+//	for (i = 0; i < ROWSIZE; i++)
+//		for (j = 0; j < COLSIZE; j++)
+//			scanf_s("%d", &score[i][j]);
+//}
+//
+//void output(int score[ROWSIZE][COLSIZE], int sum, int midsum, int finalsum)
+//{
+//	printf("---------------------------\n");
+//	printf("\n평균: %7.2f %7.2f\n", (double)midsum / ROWSIZE, (double)finalsum / ROWSIZE);
+//	printf("성적의 합은 %d이고 평균은 %.2f이다\n", sum, (double)sum / (ROWSIZE * COLSIZE));
+//}
+
+// 실습예제 7-6
+//#include<stdio.h>
+//
+//#define ARYSIZE 5
+//double sum(double ary[], int n);
+//void input(double data[]);
+//
+//int main(void)
+//{
+//	int i = 0, total = 0;
+//
+//	double data[ARYSIZE] = { 0 };
+//
+//	/*printf("5개의 데이터 입력 :");
+//	for (i = 0; i < ARYSIZE; i++)
+//		scanf_s("%lf", &data[i]);
+//	puts("");*/
+//	input(data);
+//
+//	for (i = 0; i < ARYSIZE; i++)
+//		printf("%5.1f", data[i]);
+//	puts("");
+//		
+//	printf("합 : %lf", sum(data, ARYSIZE));
+//}
+//
+//double sum(double ary[], int n)
+//{
+//	int i = 0;
+//	double total = 0;
+//
+//	for (i = 0; i < n; i++)
+//		total += ary[i];
+//
+//	return total;
+//}
+//
+//void input(double data[])
+//{
+//	int i = 0;
+//
+//	printf("5개의 데이터 입력 :");
+//	for (i = 0; i < ARYSIZE; i++)
+//		scanf_s("%lf", &data[i]);
+//	puts("");
+//}
+
+// 실습예제 7-8
 #include<stdio.h>
+
+double readary(double data[], int n);
+double sum(double data[], int n);
+void printary(double data[], int n);
 
 int main(void)
 {
-	double nums[5];                                 // 배열함수 원소 5개 0~4까지
-	double max = 0;                                 // 최대값 변수
 	int i = 0;
+	double data[5] = { 0 };
 
-	printf("다섯 개의 숫자를 입력하세요: ");
-	
-	for (i = 0; i < 5; i++)							// 초기값(i) 0, 총 5번 반복하고 반복시 초기값(i) 1씩 증가
-		scanf_s("%lf", &nums[i]);                   
-	
-	for (i = 0; i < 5; i++)							
-		if (max < nums[i])							// 처음 입력받은 값을 최대값이라 가정하고, 2번째부터 5번째까지 입력되는 값이랑 비교
-			max = nums[i];							// 처음 입력받은 값과 이후에 입력 받은 값을 비교해서 최대값을 구함
+	int arysize = sizeof(data) / sizeof(data[0]);
 
-	printf("가장 큰 값은 %.2lf", max);
+	printf("실수 5개의 값을 입력하세요.\n");
+	readary(data, arysize);
+	printf("\n입력한 자료 값은 다음과 같습니다.\n");
+	printary(data, arysize);
+	printf("\n함수에서 구한 값은 %.3lf입니다.\n", sum(data, arysize));
 
 	return 0;
+}
+
+double readary(double data[], int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		printf("data[%d] = ", i);
+		scanf_s("%lf", &data[i]);
+	}
+	return 0;
+}
+
+double sum(double data[], int n)
+{
+	int i = 0;
+	double tot = 0;
+	for (i = 0; i < n; i++)
+		tot += data[i];
+	return tot;
+}
+
+void printary(double data[], int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+		printf("data[%d] = %.2lf  ", i, data[i]);
 }

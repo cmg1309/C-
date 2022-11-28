20672db026683826ada08ab7533bed6f531f7557
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-28 16:38:00 +0900

    Update Form1.cs

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -24,7 +24,7 @@ namespace WindowsFormsApp1
 
         private void button5_Click(object sender, EventArgs e)
         {
-            button2.SendToBack();
+            button2.SendToBack(); 
         }
     }
 }

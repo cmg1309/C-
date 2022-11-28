bfceb0017f104668a21fba4a8fae292f89907a1c
cmg1309 <102665805+cmg1309@users.noreply.github.com>
2022-11-28 21:48:14 +0900

    Update 실습과제 1번.c

--- 실습과제 1번.c
+++ 실습과제 1번.c
@@ -6,7 +6,7 @@ int main(void)
 
 	printf("½Ç¼ö1 ÀÔ·Â?:");
 	scanf_s("%lf", &n1);
-	printf("½Ç¼ö2 ÀÔ·Â?:"); 
+	printf("½Ç¼ö2 ÀÔ·Â?:");  
 	scanf_s("%lf", &n2);
 	printf("%7.2lf / %7.2lf = %15.2lf", n1, n2, n1 / n2);
 
